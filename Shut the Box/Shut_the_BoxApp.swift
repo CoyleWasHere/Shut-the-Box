00f6d08b37e7322129accb6e353a169cf6f5ad19
CoyleWasHere <dancoyle321@gmail.com>
2024-03-18 20:47:35 -0400

    Initial Commit

---
+++ Shut the Box/Shut_the_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shut_the_BoxApp.swift
+//  Shut the Box
+//
+//  Created by Daniel Coyle on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Shut_the_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
